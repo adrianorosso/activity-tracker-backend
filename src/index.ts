10476ac1f87c87e617ad39eca88306e38a3b8286
Adriano Rosso <rosso.af@gmail.com>
2021-05-15 19:25:57 -0300

    create simple CRUD for activity

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
 import "reflect-metadata";
-// import { createConnection } from "typeorm";
+import { createConnection } from "typeorm";
 import { createRoutes } from "./routes";
 
-const main = () => {
-  // createConnection({
-  //   type: "postgres",
-  //   host: "localhost",
-  //   port: 5432,
-  //   username: "postgres",
-  //   password: "postgres",
-  //   database: "activities_db",
-  //   entities: [__dirname + "/entity/*.js"],
-  //   logging: true,
-  //   synchronize: true,
-  // })
-  //   .then((connection) => {
-  //     // here you can start to work with your entities
-  //     console.log("Creating tables");
-  //   })
-  //   .catch((error) => console.log(error));
+const main = async () => {
+  let connection;
 
-  createRoutes();
+  try {
+    connection = await createConnection({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "postgres",
+      database: "activities_db",
+      entities: [__dirname + "/entity/*.js"],
+      logging: true,
+      synchronize: true,
+    });
+  } catch (error) {
+    console.log(error);
+    throw Error("connection failed");
+  }
+
+  createRoutes(connection);
 };
 
 main();
--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,62 @@
 import express from "express";
+import { Connection, getConnection, getRepository } from "typeorm";
+import { Activity } from "./entity/activity";
+import { IActivity } from "./interface/activity";
 
-export const createRoutes = () => {
+export const createRoutes = (dbConnection: Connection) => {
   const app = express();
   app.use(express.json());
 
   const PORT = 3000;
 
-  app.use("/", (_, res) => {
-    res.send("OK");
+  app.get("/activity", async (_, res) => {
+    const activities = await dbConnection.getRepository(Activity).find();
+    res.send({ activities: activities });
+  });
+
+  app.post("/activity", async (req, res) => {
+    const newActivity = new Activity();
+    const { name, description } = req.body as IActivity;
+
+    newActivity.name = name;
+    newActivity.description = description;
+    newActivity.creationDate = new Date().toISOString();
+
+    const saved = await newActivity.save();
+
+    res.send({ created: saved });
+  });
+
+  app.put("/activity/:id", async (req, res) => {
+    const { name, description } = req.body as IActivity;
+    const id = req.params.id;
+
+    await getConnection()
+      .createQueryBuilder()
+      .update(Activity)
+      .set({ name: name, description: description })
+      .where("id = :id", { id: id })
+      .execute();
+
+    const updated = await getRepository(Activity)
+      .createQueryBuilder("act")
+      .where("act.id = :id", { id: id })
+      .getOne();
+
+    res.send({ updated: updated });
+  });
+
+  app.delete("/activity/:id", async (req, res) => {
+    const repo = await dbConnection.getRepository(Activity);
+    const element = await repo.findOne(req.params.id);
+    if (element) {
+      const removed = await repo.remove(element);
+      res.send({ removed: removed });
+    }
+  });
+
+  app.use((_, res) => {
+    res.sendStatus(404).send("address not found");
   });
 
   app.listen(PORT, () => {
